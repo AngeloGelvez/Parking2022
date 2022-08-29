b338d75a2dade342843d814584215edae41c7198
AngeloGelvez <111929278+AngeloGelvez@users.noreply.github.com>
2022-08-28 21:33:44 -0500

    Cambios
    Se añadio el index front y paquetes con clases en el back

---
+++ src/main/java/com/turboparking/controladores/ControladorPrincipal.java
@@ -0,0 +1,24 @@
+package com.turboparking.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //Indico que esta clase va actuar como Controlador, Es decir una clase Controlador
+public class ControladorPrincipal {
+    
+    // URL http://localhost:8080/
+    @GetMapping("/") 
+    /*
+    *Esto hace que la pagina web arranque con el archivo index.html, es decir
+    que con la URL que indicamos en GetMapping ("/") arranca mi archivo index
+    */
+    public String cargarIndex() {
+        return "index";
+    }
+    
+    // URL http://localhost:8080/login
+    @GetMapping("/login")
+    public String cargarInicioSesion() {
+        return "iniciosesion";
+    }
+}
